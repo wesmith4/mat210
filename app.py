55733fc9f7a8c757f49bd488e02377c48266b444
Will Smith <william.e.smithiv@gmail.com>
2021-04-10 16:47:14 -0400

    try out altair

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import plotly.express as px
+import altair as alt
+import numpy as np
+import pandas as pd
 
 import hw6.enrollment as enrollment
 
@@ -13,3 +16,17 @@ st.sidebar.title("Navigation")
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
+
+x = np.arange(100)
+source = pd.DataFrame({
+  'x': x,
+  'f(x)': np.sin(x / 5)
+})
+
+chart = alt.Chart(source).mark_line().encode(
+    x='x',
+    y='f(x)'
+)
+
+st.altair_chart(chart)
+
