4252fb3fc5e70b80499eae783f2c0b1d7c2ec785
Will Smith <william.e.smithiv@gmail.com>
2021-04-17 11:17:43 -0400

    add home page to app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import altair as alt
 import numpy as np
 import pandas as pd
+import home
 
 st.set_page_config(page_title="MAT210 - Will Smith")
 
@@ -13,12 +14,13 @@ import hw6.enrollment as enrollment
 import hw8.NBAclustering as NBAclustering
 
 PAGES = {
+    "Home": home,
     "School Enrollment": enrollment,
     "NBA Clustering": NBAclustering
 }
 
 st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()),index=1)
+selection = st.sidebar.radio("Go to", list(PAGES.keys()),index=0)
 page = PAGES[selection]
 page.app()
 
---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+def app():
+    st.title("MAT 210 - Will Smith")
+    
+    st.markdown("""
+    MAT210 course taken in Spring 2021 with Dr. Tim Chartier at Davidson College.
+
+    Visit the pages listed in the sidebar to see visualizations and analysis from various notes and assignments.
+
+
+
+    """)
